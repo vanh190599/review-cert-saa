023256cfeeb215dacc1c450af2d3058c32b605c6
anhnv <anh195np@gmail.com>
2023-12-18 23:13:56 +0700

    develop

---
+++ main.js
@@ -0,0 +1,16 @@
+function switchMode(el) {
+    const body = $('body');
+    const theme = $('#theme_name');
+
+    let mode = body.attr('data-bs-theme');
+    if (mode === 'dark') {
+        body.attr('data-bs-theme', 'light');
+        theme.html('Light');
+        return;
+    }
+    body.attr('data-bs-theme', 'dark');
+    theme.html('Dark');
+}
+
+
+
