a4cf5c0ecbd85fd74cefda2bddec60dee5658e44
anhnv <anh195np@gmail.com>
2023-12-20 23:36:22 +0700

    develop

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function switchMode(el) {
     const body = $('body');
-    const theme = $('#theme_name');
+    // const theme = $('#theme_name');
 
     let mode = body.attr('data-bs-theme');
     if (mode === 'dark') {
         body.attr('data-bs-theme', 'light');
-        theme.html('Light');
+        // theme.html('Light');
         return;
     }
 
     body.attr('data-bs-theme', 'dark');
-    theme.html('Dark');
+    // theme.html('Dark');
 }
 
 $('#header-mb .nav-link').on('click', function () {
